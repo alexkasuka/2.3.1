fad6b6a5f1940fdec07790510781cb0ca85de536
Alexey Zarubin <alzarubin@gmail.com>
2022-09-20 19:31:18 +0300

    Initial commit

---
+++ src/main/java/web/controller/PeopleController.java
@@ -0,0 +1,66 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import web.model.Person;
+import web.service.PeopleService;
+
+import javax.validation.Valid;
+
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PeopleService peopleService;
+
+
+
+    @Autowired
+    public PeopleController(PeopleService peopleService) {
+        this.peopleService = peopleService;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+
+        model.addAttribute("people", peopleService.findAll());
+
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", peopleService.find(id));
+        return "people/show";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+
+        peopleService.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", peopleService.find(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+
+        peopleService.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        peopleService.delete(id);
+        return "redirect:/people";
+    }
+}
